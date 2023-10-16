8910c1bf45c1f6394ccab669c2af675374663391
Aydar N. (Sublime Merge MBP) <me@aydar.media>
2023-10-16 21:54:25 +0300

    Added jquery

--- src/script.js
+++ src/script.js
@@ -6,6 +6,7 @@ me@aydar.media
 *************************/
 
 import './style.css'
+import $ from 'jquery';
 
 const CMYK = [
 	'#00AAE9',
@@ -21,14 +22,17 @@ const MIX = [
 ]
 
 let flocks = []
+let headings = []
+
+let $container, $frame
 
 const createCell = (size, color) => {
-	const cell = document.createElement('picture');
-	cell.style.width = size + 'px';
-	cell.style.height = size + 'px';
-	cell.style.backgroundColor = color
-	cell.className = 'cell';
-	return cell;
+	const $cell = $('<picture></picture>')
+	$cell.css('width', size + 'px');
+	$cell.css('height', size + 'px');
+	$cell.css('backgroundColor', color);
+	$cell.addClass('cell')
+	return $cell;
 }
 
 const createRandomColor = (colorDict, index) => {
@@ -38,26 +42,48 @@ const createRandomColor = (colorDict, index) => {
 const createFlock = (id, cellSize, rows, columns, color) => {
 	const [width, height] = [(cellSize * columns) + 'px', (cellSize * rows) + 'px']
 
-	const container = document.getElementById('canvas');
-	const frame = document.getElementById('frame');
+	const $container = $('#canvas');
+	const $frame = $('#id')
 
-	const flock = document.createElement('div')
-	flock.className = flocks.length === 0 ? 'flock primary' : 'flock'
-	flock.id = id
-	flock.style.width = width; frame.style.width = width
-	flock.style.height = height; frame.style.height = height
+	const $flock = $('<div></div>');
+	$flock.addClass(flocks.length === 0 ? 'flock primary' : 'flock')
+	$flock.id = id
+	$flock.css('width', width); $frame.css('width', width)
+	$flock.css('height', height); $frame.css('height', height);
 
 	for (let y = 0; y < rows; y++) {
 		for (let x = 0; x < columns; x++) {
-			const square = createCell(cellSize, Array.isArray(color) ? color[(y + 1) * x] : createRandomColor(color, x + y));
-			square.style.left = x * cellSize + 'px';
-			square.style.top = y * cellSize + 'px';
-			flock.appendChild(square);
+			const $cell = createCell(cellSize, Array.isArray(color) ? color[(y + 1) * x] : createRandomColor(color, x + y));
+			$cell.css('left', x * cellSize + 'px');
+			$cell.css('top', y * cellSize + 'px');
+			$flock.append($cell);
 		}
 	}
 
-	flocks.push(flock)
-	container.appendChild(flock)
+	flocks.push($flock)
+	$container.append($flock)
+}
+
+const calc = (percent, px) => {
+	const tail = px >= 0 ? `+ ${px}` : `- ${Math.abs(px)}`
+	return `calc(${percent}% ${tail}px)`
+}
+
+const createHeading = (text, x, y, additionalStyles = null) => {
+	const $heading = $('<h1></h1>')
+	$heading.html(text)
+	$heading.css('top', calc(50, y))
+	$heading.css('left', calc(50, x))
+	console.log(calc(50, x))
+	for (let i in additionalStyles) $heading.css(i, additionalStyles[i]);
+
+	$container.append($heading)
+
+	const $stroke = $heading.clone(true)
+	$stroke.addClass('stroke')
+	$container.append($stroke)
+
+	headings.push($heading)
 }
 
 Array.prototype.random = function () {
@@ -65,5 +91,10 @@ Array.prototype.random = function () {
 }
 
 window.addEventListener('load', () => {
-	createFlock('s', 60, 10, 20, { array: CMYK, makeCheckers: true })
+	$container = $('#canvas');
+	$frame = $('#frame');
+
+	createFlock('s', 80, 8, 16, { array: CMYK, makeCheckers: true })
+	createHeading('общее и частное<br>московских таксистов', -590, -260)
+	createHeading('сквозь оформление<br>машин такси', -110, 150, { textAlign: 'right' })
 });
